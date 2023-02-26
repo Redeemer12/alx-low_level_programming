ae99db500df1dbaf54dc02268bebdb0437a8304e
Redeemer12 <2017radawu@uhas.edu.gh>
2023-02-25 21:46:16 -0800

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stidio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwars 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
